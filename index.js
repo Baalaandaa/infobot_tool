d4d4139945a4cdbdf079eef26083157797225bd4
baalaandaa <baalaandaa@gmail.com>
2020-08-05 00:42:57 +0300

    0.0.1

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const config = require('./config');
 
 const prompts = require('./prompts/index')
 
-figlet.text("Infobot", {}, async (e, d) => {
-    console.log(chalk.blue(d));
-    console.log(chalk.yellow(`Current version: ${version}`));
-    if(!config.get('key')) config.set('key', await prompts.apiKey());
-    if(!config.get('secret')) config.set('secret', await prompts.apiSecret());
-});
+module.exports = () => {
+    figlet.text("Infobot", {}, async (e, d) => {
+        console.log(chalk.blue(d));
+        console.log(chalk.yellow(`Current version: ${version}`));
+        if (!config.get('key')) config.set('key', await prompts.apiKey());
+        if (!config.get('secret')) config.set('secret', await prompts.apiSecret());
+    });
+}
 
 
